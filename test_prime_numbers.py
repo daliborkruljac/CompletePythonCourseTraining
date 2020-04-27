964bf8a9dbb67fbe21bbf64309851513699901c4
daliborkruljac <dalibor.kruljac@gmail.com>
2020-04-28 00:10:24 +0200

    prime_numbers

---
+++ test_prime_numbers.py
@@ -0,0 +1,10 @@
+#prime numbers
+
+for n in range (2,101):
+    for x in range (2,n):
+        if n % x == 0:
+            print(f'{n} equals {x} * {n/x}')
+            break
+    else:
+        print(f'{n} is prime number')
+    
