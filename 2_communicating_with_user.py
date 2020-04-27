7a045180f641319d6c87876acb760a344548eccc
daliborkruljac <dalibor.kruljac@gmail.com>
2020-04-27 14:14:50 +0200

    communicating

---
+++ 2_communicating_with_user.py
@@ -0,0 +1,8 @@
+# First, ask the user for their name. Then, print out the greeting "Hello, NAME"
+# Remember the uppercase H, the comma, and the space between words!
+
+# Secondly, ask the user for their age and convert it into a number.
+# Then, print out how many months that equals to (you'll have to multiply the age by 12).
+
+name = input ('Enter your name: ')
+print ('Hello, ' + name)
