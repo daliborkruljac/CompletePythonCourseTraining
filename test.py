37415e34a4438d91a41cb604ac802c1998f15ba8
daliborkruljac <dalibor.kruljac@gmail.com>
2020-04-28 00:27:51 +0200

    test

--- test.py
+++
@@ -1,9 +0,0 @@
-friend = input("Enter your friend name: ")
-friends = ["Rolf", "Bob", "Jen", "Charlie", "Anne"]
-friends_lower = [name.lower() for name in friends]
-
-if friend.lower() in friends_lower:
-    print(f"I know {friend.title()}!")
-else:
-    print(f'I don\'t know {friend.title()}')
-    
