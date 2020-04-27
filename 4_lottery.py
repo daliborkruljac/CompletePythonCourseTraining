f487498b420c1b9c907f6455d1d8ca1bd1e5899f
daliborkruljac <dalibor.kruljac@gmail.com>
2020-04-27 16:45:32 +0200

    lottery solved

--- 4_lottery.py
+++ 4_lottery.py
@@ -11,7 +11,16 @@ A player looks like this:
 
 Define a list with two players (you can come up with their names and numbers).
 """
-players = []
+players = [
+    {
+        'name': 'Dejan',
+        'numbers' : {3,8,12,21,29}
+    },
+    {
+        'name': 'Danijel',
+        'numbers' : {5,9,12,21,22}
+    }
+]
 
 """
 For each of the two players, print out a string like this: "Player PLAYER_NAME got 3 numbers right.".
@@ -21,3 +30,11 @@ Then construct a string and print it out.
 
 Remember: the string must contain the player's name and the amount of numbers they got right!
 """
+
+name = players[0]['name']
+numbers = players[0]['numbers'].intersection(lottery_numbers)
+print('Player {name} got {amount} numbers right.'.format(name=name, amount=len(numbers)))
+ 
+name = players[1]['name']
+numbers = players[1]['numbers'].intersection(lottery_numbers)
+print('Player {name} got {amount} numbers right.'.format(name=name, amount=len(numbers)))
