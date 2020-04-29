2344d6b7b9c6bd655c84bcb03a749dee65f7b59c
daliborkruljac <dalibor.kruljac@gmail.com>
2020-04-29 11:08:42 +0200

    tests

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-friends = ['Rolf', 'ruth','charlie','Jen']
-guests = ['jose','Bob','Rolf','Charlie','michael']
-
-present_friends = [
-    name.title() for name in guests if name.title() in [f.title() for f in friends]
-]
-
-print(present_friends)
+#test
