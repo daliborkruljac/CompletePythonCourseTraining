dd1e507ff1c6ae0591eba953386dd50f31692374
daliborkruljac <dalibor.kruljac@gmail.com>
2020-04-29 10:03:23 +0200

    comment

--- test_lists_comprehensions.py
+++ test_lists_comprehensions.py
@@ -4,5 +4,6 @@ guests = ['jose','Bob','Rolf','Charlie','michael']
 present_friends = [
     name.title() for name in guests if name.title() in [f.title() for f in friends]
 ]
+#don't do this usually as this makes code harder to read/understand
 
 print(present_friends)
