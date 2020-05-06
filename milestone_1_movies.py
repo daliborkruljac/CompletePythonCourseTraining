ed1755a90ff00b7da8a4e3032af7a65311712d1f
daliborkruljac <dalibor.kruljac@gmail.com>
2020-05-06 10:15:42 +0200

    started changing milestone project

--- milestone_1_movies.py
+++ milestone_1_movies.py
@@ -1,39 +1,45 @@
 # Incomplete app!
-
-MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
+# Define movies as a list:
+# Save movies to text file later (version 2, use dataabase in version 3)
 movies = []
+selection = ''
 
-
-# You may want to create a function for this code
-title = input("Enter the movie title: ")
-director = input("Enter the movie director: ")
-year = input("Enter the movie release year: ")
-
-movies.append({
-    'title': title,
-    'director': director,
-    'year': year
-})
-
-
-# Create other functions for:
-#   - listing movies
-#   - finding movies
-
-
-# And another function here for the user menu
-selection = input(MENU_PROMPT)
-while selection != 'q':
-    if selection == "a":
-        pass
-    elif selection == "l":
-        pass
-    elif selection == "f":
-        pass
-    else:
-        print('Unknown command. Please try again.')
-
+#function for Menu Prompt
+def MenuPrompt():  
+    MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
     selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == "a":
+            MovieInput()
+        elif selection == "l":
+            MovieList()             
+        elif selection == "f":
+            MovieFind()
+        else:
+            print('Unknown command. Please try again.')
+
+
+# function for Movie Input
+def MovieInput ():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+    })
+    selection == ''             #change selection to avoid infinite loop
+
+# function for Movie Listing
+def MovieList():
+    pass
+
+# function for Movie Finding
+def MovieFind():
+    pass
+
+MenuPrompt()
 
 
 # Remember to run the user menu function at the end!
