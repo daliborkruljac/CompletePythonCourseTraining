5277d9554c66cd4b44a347137f43b8ca4683e046
daliborkruljac <dalibor.kruljac@gmail.com>
2020-05-06 18:38:01 +0200

    updated_9_method

--- 9_defining_a_method.py
+++ 9_defining_a_method.py
@@ -1,5 +1,5 @@
 # We've already defined a movie class like this:
-class Movie:
+class MovieListing:
     def __init__(self, new_name, new_director):
         self.name = new_name
         self.director = new_director
@@ -9,8 +9,12 @@ class Movie:
         print(f'<<{self.name}>> by {self.director}')
 
 # You only need to finish the method, we will take care of the object creation and call your method for you!
-'''
-my_movie = Movie('The Red Car','Paco')
 
-my_movie.print_info()
-'''
+# Code below is not needed in the taks
+
+my_movie = MovieListing ('The Matrix', 'Wachowski')
+
+MovieListing.print_info(my_movie)
+
+
+
