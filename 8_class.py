a9863eea7ec831d221f950fef3ede544d677d395
daliborkruljac <dalibor.kruljac@gmail.com>
2020-05-06 15:08:26 +0200

    8_class_done

---
+++ 8_class.py
@@ -0,0 +1,13 @@
+# Define a Movie class that has two properties: name and director
+
+class Movie:
+    def __init__ (self, newname, newdirector):
+        self.name = newname
+        self.director = newdirector
+
+# You should be able to create Movie objects like this one:
+my_movie = Movie('The Matrix', 'Wachowski')
+
+
+
+
