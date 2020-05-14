8e658c20a149ba6d35b0e27fd5591f9feb11ce8b
daliborkruljac <dalibor.kruljac@gmail.com>
2020-05-14 11:48:44 +0200

    14_quiz started

---
+++ 14_quiz.py
@@ -0,0 +1,14 @@
+"""
+sample `questions.txt` file:
+1+1=2
+2+2=4
+8-4=4
+task description:
+- read from `questions.txt`
+- for each question, print out the question and and wait for the user's answer
+    for example, for the first question, print out: `1+1=`
+- after the user answers all the questions, calculate her score and write it to the `result.txt` file
+    the result should be in such format: `Your final score is n/m.`
+    where n and m are the number of correct answers and the maximum score respectively
+"""
+# your code starts here:
