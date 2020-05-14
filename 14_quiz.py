ff94cde656e5a6c9e6dad7bb656ede9b6fbf1155
daliborkruljac <dalibor.kruljac@gmail.com>
2020-05-14 13:44:29 +0200

    finished quiz

--- 14_quiz.py
+++ 14_quiz.py
@@ -12,3 +12,29 @@ task description:
     where n and m are the number of correct answers and the maximum score respectively
 """
 # your code starts here:
+
+guessed = 0
+numberofquestions = 0
+
+quiz = open('questions.txt', 'r')
+            #each question is a str "1+1=2\n"
+
+for line in quiz:
+    quizline = line.rstrip("\n")        #remove the line break at the end
+    quizline = quizline.split('=')      #now our line is a list ['1+1','2']
+    numberofquestions +=1            #counting number of questions
+    #Let's show user first list member, then ask for second and compare second 
+    useranswer = input (f'{quizline[0]}: ')
+    if useranswer == quizline[1]:
+        guessed += 1
+
+print (f'You final score is {guessed}/{numberofquestions}.')
+
+#let's print to file also
+results = open('results.txt','w+')
+results.write (f'You final score is {guessed}/{numberofquestions}.')
+results.close()
+
+'''
+
+'''
