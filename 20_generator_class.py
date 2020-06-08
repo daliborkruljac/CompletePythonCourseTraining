4a029b0f058ea6f5fe74ade3efc9e59cf4060185
daliborkruljac <dalibor.kruljac@gmail.com>
2020-06-08 15:11:16 +0200

    21 added

--- 20_generator_class.py
+++ 20_generator_class.py
@@ -1,7 +1,6 @@
 
 # Define a PrimeGenerator class
 class PrimeGenerator:
-    # You may modify the __init__() method if necessary, but you don't need to change its arguments
     def __init__(self, stop):
         self.stop = stop                            # stop defines the range (exclusive upper bound) in which we search for the primes
         self.start = 2                              # start defines first number to work with
@@ -18,22 +17,8 @@ class PrimeGenerator:
                 return n                            #return n for this round
         raise (StopIteration)                       #this is what tells Python we've reached the end of the generator
 
-'''
-#need to define __iter__ to return itself like any iterable function (don't know how)
-
-
     def __iter__(self):
         return self
 
-
-
-
-#TEST
-#Please don't go much over 10000
-
-g = PrimeGenerator(77)
+g = PrimeGenerator(30)
 print(list(g))
-
-'''
-
-
---
+++ 21_collections.py
@@ -0,0 +1,45 @@
+from collections import defaultdict, OrderedDict, namedtuple, deque
+
+
+def task1() -> defaultdict:
+    """
+    - create a `defaultdict` object, and its default value would be set to the string `Unknown`.
+    - Add an entry with key name `Alan` and its value being `Manchester`.
+    - Return the `defaultdict` object you created.
+    """
+    # you code starts here:
+    pass
+
+
+def task2(arg_od: OrderedDict):
+    """
+    - takes in an OrderedDict `arg_od`
+    - Remove the first and last entry in `arg_od`.
+    - Move the entry with key name `Bob` to the end of `arg_od`.
+    - Move the entry with key name `Dan` to the start of `arg_od`.
+    - You may assume that `arg_od` would always contain the keys `Bob` and `Dan`,
+        and they won't be the first or last entry initially.
+    """
+    # you code starts here:
+    pass
+
+
+def task3(name: str, club: str) -> namedtuple:
+    """
+    - create a `namedtuple` with type `Player`, and it will have two fields, `name` and `club`.
+    - create a `Player` `namedtuple` instance that has the `name` and `club` field set by the given arguments.
+    - return the `Player` `namedtuple` instance you created.
+    """
+    # you code starts here:
+    pass
+
+
+def task4(arg_deque: deque):
+    """
+    - Manipulate the `arg_deque` in any order you preferred to achieve the following effect:
+        -- remove last element in `deque`
+        -- move the fist (left most) element to the end (right most)
+        -- add an element `Zack`, a string, to the start (left)
+    """
+    # you code starts here:
+    pass
